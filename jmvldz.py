237786ab99ced64fe501f17d1721258f0e328610
Josh Valdez <exxonvaldeez@gmail.com>
2013-02-01 03:18:50 -0800

    Added flat pages

--- jmvldz.py
+++ jmvldz.py
@@ -4,20 +4,24 @@
 # imports
 import stripe, os
 from flask import Flask, request, render_template
-from flask_flatpages import FlatPages
+from flask_flatpages import FlatPages, pygments_style_defs
+
+# config - TODO
+COMPANY = {'name': 'Joshua Miles Valdez'}
+STRIPE_KEYS_T = { 'secret_key': 'sk_test_di8yqIu1WG4SS1HnLKmxhHEq', 'publishable_key': 'pk_test_4favssQkxAyb333FIyM1Ybjr' }
+STOLLER = {'name': 'Elliot', 'amount': '250', 'cents': '25000', 'description': 'Amends Website Upgrade'}
+
+# application
+app = Flask(__name__)
+app.config.from_pyfile('config/jmvldz.cfg')
+pages = FlatPages(app)
 
 # stripe config
 STRIPE_KEYS_S = {
     'secret_key': os.environ['SECRET_KEY'],
     'publishable_key': os.environ['PUBLISHABLE_KEY']
 }
-
-# application
-app = Flask(__name__)
-#app.config.from_object(__name__)
-app.config.from_pyfile('config/jmvldz.cfg')
-
-#stripe.api_key = STRIPE_KEYS_S['secret_key']
+stripe.api_key = STRIPE_KEYS_S['secret_key']
 
 # helper functions
 def cents(amount):
@@ -26,15 +30,15 @@ def cents(amount):
 # routes
 @app.route('/')
 def show_index():
-    return render_template('index.html')
+    return render_template('portfolio/index.html')
 
 @app.route('/mind')
 def show_vis():
-    return render_template('retweet_network.html')
+    return render_template('portfolio/retweet_network.html')
 
 @app.route('/stoller')
 def stoller():
-    return render_template('payment.html', key = STRIPE_KEYS_S['publishable_key'],
+    return render_template('payments/payment.html', key = STRIPE_KEYS_S['publishable_key'],
                            client = STOLLER, company = COMPANY)
 
 @app.route('/charge', methods = ['POST'])
@@ -49,7 +53,24 @@ def charge():
         currency = 'usd',
         description = STOLLER['description'])
 
-    return render_template('charge.html', client = STOLLER)
+    return render_template('payments/charge.html', client = STOLLER)
+
+@app.route('/pygments.css')
+def pygments_css():
+    return pygments_style_defs('monokai'), 200, {'Content-Type': 'text/css'}
+
+@app.route('/log')
+def logs():
+    # Articles are pages with a publication date
+    articles = (p for p in pages if 'published' in p.meta)
+    # Show the 10 most recent articles, most recent first.
+    latest = sorted(articles, reverse=True,
+                    key=lambda p: p.meta['published'])
+    return render_template('log/articles.html', articles=latest[:10])
+
+@app.route('/marketing')
+def marketing():
+    return render_template('log/marketing-narrow.html')
 
 
 # main
---
+++ pages/hello.md
@@ -0,0 +1,10 @@
+title: Hello
+published: 2013-01-27
+
+Hello World
+===========
+
+Hello world!
+
+`#This is code`
+`print 'Hello World'`
